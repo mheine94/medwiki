2910d71aa6392461762267ca963cccfefbf95933
Maurice Heine <maurice@pop-os.localdomain>
2020-07-27 01:39:46 +0200

    port

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const fetch = require("node-fetch")
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
-const port = 1337
+const port = 80
 
 app.use(express.static('public'))
 app.use(bodyParser.json());
