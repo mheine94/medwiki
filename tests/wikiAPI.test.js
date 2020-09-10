bb7380d5b59bfa8097f34370e96d6cccd52a2e8d
Maurice <maurice.heine@uni-muenster.de>
2020-09-10 20:43:03 +0200

    Add mapToResult Tests

--- tests/wikiAPI.test.js
+++ tests/wikiAPI.test.js
@@ -1,9 +1,10 @@
-const {createInnDict} = require('../src/api/wikiAPI')
+const {createInnDict, mapToResult} = require('../src/api/wikiAPI')
 const {data} = require('./wikiApi-test-data')
+const Queue = require('bull')
+jest.mock('bull')
 
-describe('WikiAPI tests', ()=>{
+describe('INNDict tests', ()=>{
     test('ceate INNDict with two mediactions of the same inn', ()=>{
-        console.log("Bongpo")
         expect(createInnDict(data.sameInn.input)).toEqual(data.sameInn.output)        
     })
     test('create INNDict with two medication of different inns', ()=>{
@@ -15,4 +16,28 @@ describe('WikiAPI tests', ()=>{
     test('create INNDict with known and unknow inn entries', ()=>{
         expect(createInnDict(data.unknownInnAndKnown.input)).toEqual(data.unknownInnAndKnown.output)
     })
+})
+
+describe('mapToResult tests',()=>{
+   test('queue functionss are called and awaited and result as expected', async ()=>{
+        let mockFinish =  jest.fn(()=>{return Promise.resolve("success")})
+        let mockAdd =  jest.fn(function() {  let res = Promise.resolve(
+                    {
+                        finished:()=> mockFinish()
+                    })
+                return res
+        })
+       Queue.mockImplementation(()=>
+       {
+        return  {
+            add : ()=>{return mockAdd()}
+        }
+       })
+      let promises = mapToResult(["Amoxicillin","Nurofen"], "de")
+      let result = await Promise.all(promises)
+      expect(result).toEqual([{query:"Amoxicillin",result:"success"},{query:"Nurofen",result:"success"}])
+      expect(Queue).toHaveBeenCalledTimes(1);   
+      expect(mockAdd).toHaveBeenCalledTimes(2);
+      expect(mockFinish).toHaveBeenCalledTimes(2);
+   }) 
 })
