7688dd4e1c6aace1517236344b3e10968331717a
Maurice <maurice.heine@uni-muenster.de>
2020-09-10 18:12:49 +0200

    Added INNDict tests

--- tests/wikiAPI.test.js
+++ tests/wikiAPI.test.js
@@ -2,8 +2,17 @@ const {createInnDict} = require('../src/api/wikiAPI')
 const {data} = require('./wikiApi-test-data')
 
 describe('WikiAPI tests', ()=>{
-    test('ceate INNDict with two entries', ()=>{
+    test('ceate INNDict with two mediactions of the same inn', ()=>{
         console.log("Bongpo")
         expect(createInnDict(data.sameInn.input)).toEqual(data.sameInn.output)        
+    })
+    test('create INNDict with two medication of different inns', ()=>{
+        expect(createInnDict(data.diffrentInn.input)).toEqual(data.diffrentInn.output)
+    })
+    test('create INNDict with unknown value should create unknown array', ()=>{
+        expect(createInnDict(data.unknownInn.input)).toEqual(data.unknownInn.output)
+    })
+    test('create INNDict with known and unknow inn entries', ()=>{
+        expect(createInnDict(data.unknownInnAndKnown.input)).toEqual(data.unknownInnAndKnown.output)
     })
 })
--- tests/wikiApi-test-data.js
+++ tests/wikiApi-test-data.js
@@ -79,73 +79,168 @@ module.exports = {
             }
         },
         diffrentInn:{
-            "ibuprofen": {
-                "inn": "ibuprofen",
-                "ingredientClass": [
-                    "Nichtsteroidales Antirheumatikum"
-                ],
-                "tradenames": [
-                    "Nurofen"
-                ],
-                "formula": [
-                    "C13H18O2"
-                ],
-                "cas": [
-                    "15687-27-1"
-                ],
-                "atc": [
-                    "M02AA13",
-                    "M01AE01",
-                    "C01EB16",
-                    "G02CC01"
-                ]
-            },
-            "amoxicillin": {
-                "inn": "amoxicillin",
-                "ingredientClass": [
-                    "β-Lactam-Antibiotika"
-                ],
-                "tradenames": [],
-                "formula": [
-                    "C16H19N3O5S"
-                ],
-                "cas": [
-                    "26787-78-0",
-                    "61336-70-7 (Amoxicillin-Trihydrat)",
-                    "34642-77-8 (Amoxicillin-Natrium)"
-                ],
-                "atc": [
-                    "J01CA04"
-                ]
+            input:[
+                {
+                    query:"Nurofen",
+                    result:{
+                        "inn": "ibuprofen",
+                        "ingredientClass": [
+                            "Nichtsteroidales Antirheumatikum"
+                        ],
+                        "tradenames": [
+                            "Nurofen"
+                        ],
+                        "formula": [
+                            "C13H18O2"
+                        ],
+                        "cas": [
+                            "15687-27-1"
+                        ],
+                        "atc": [
+                            "M02AA13",
+                            "M01AE01",
+                            "C01EB16",
+                            "G02CC01"
+                        ]
+                    }
+                },
+                {
+
+                    query:"Amocicillin",
+                    result: {
+                        "inn": "amoxicillin",
+                        "ingredientClass": [
+                            "β-Lactam-Antibiotika"
+                        ],
+                        "tradenames": [],
+                        "formula": [
+                            "C16H19N3O5S"
+                        ],
+                        "cas": [
+                            "26787-78-0",
+                            "61336-70-7 (Amoxicillin-Trihydrat)",
+                            "34642-77-8 (Amoxicillin-Natrium)"
+                        ],
+                        "atc": [
+                            "J01CA04"
+                        ]
+                    }
+
+                }
+            ],
+            output:{
+                "ibuprofen": {
+                    "inn": "ibuprofen",
+                    "ingredientClass": [
+                        "Nichtsteroidales Antirheumatikum"
+                    ],
+                    "tradenames": [
+                        "Nurofen"
+                    ],
+                    "formula": [
+                        "C13H18O2"
+                    ],
+                    "cas": [
+                        "15687-27-1"
+                    ],
+                    "atc": [
+                        "M02AA13",
+                        "M01AE01",
+                        "C01EB16",
+                        "G02CC01"
+                    ]
+                },
+                "amoxicillin": {
+                    "inn": "amoxicillin",
+                    "ingredientClass": [
+                        "β-Lactam-Antibiotika"
+                    ],
+                    "tradenames": [],
+                    "formula": [
+                        "C16H19N3O5S"
+                    ],
+                    "cas": [
+                        "26787-78-0",
+                        "61336-70-7 (Amoxicillin-Trihydrat)",
+                        "34642-77-8 (Amoxicillin-Natrium)"
+                    ],
+                    "atc": [
+                        "J01CA04"
+                    ]
+                }
             }
         },
-        unknownInn:{
-                "unknown": [
-        "Dasistkeinmedikament"
-        ]
+        unknownInn:
+            {
+            input:[
+                {
+                    query:"Dasistkeinmedikament",
+                    result:{
+                        error: "Nothing was found on wikipedia",
+                        query: "Dasistkeinmedikament"
+                    }
+                }
+            ],
+            output:{
+                    "unknown": [
+                      "Dasistkeinmedikament"
+                    ] 
+            }
         },
         unknownInnAndKnown:{
+            input:[
+                {
+                    query:"Dasistkeinmedikament",
+                    result:{
+                        error: "Nothing was found on wikipedia",
+                        query: "Dasistkeinmedikament"
+                    }
+                },
+                {
+                    query: "Amoxicillin",
+                    result:{
+                        "inn": "amoxicillin",
+                        "ingredientClass": [
+                            "β-Lactam-Antibiotika"
+                        ],
+                        "tradenames": [],
+                        "formula": [
+                            "C16H19N3O5S"
+                        ],
+                        "cas": [
+                            "26787-78-0",
+                            "61336-70-7 (Amoxicillin-Trihydrat)",
+                            "34642-77-8 (Amoxicillin-Natrium)"
+                        ],
+                        "atc": [
+                            "J01CA04"
+                        ]
+                    }
+                }
+            ],
+            output:{
                 "unknown": [
-        "Dasistkeinmedikament"
-    ],
-    "amoxicillin": {
-        "inn": "amoxicillin",
-        "ingredientClass": [
-            "β-Lactam-Antibiotika"
-        ],
-        "tradenames": [],
-        "formula": [
-            "C16H19N3O5S"
-        ],
-        "cas": [
-            "26787-78-0",
-            "61336-70-7 (Amoxicillin-Trihydrat)",
-            "34642-77-8 (Amoxicillin-Natrium)"
-        ],
-        "atc": [
-            "J01CA04"
-        ]
-    }
+                "Dasistkeinmedikament"
+                ],
+                "amoxicillin": {
+                    "inn": "amoxicillin",
+                    "ingredientClass": [
+                        "β-Lactam-Antibiotika"
+                    ],
+                    "tradenames": [],
+                    "formula": [
+                        "C16H19N3O5S"
+                    ],
+                    "cas": [
+                        "26787-78-0",
+                        "61336-70-7 (Amoxicillin-Trihydrat)",
+                        "34642-77-8 (Amoxicillin-Natrium)"
+                    ],
+                    "atc": [
+                        "J01CA04"
+                    ]
+                }
+            }
         }
     }
 }
