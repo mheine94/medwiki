f2329a0fc3f5862d9afbc2660c59d6e4a240648d
Maurice <maurice.heine@uni-muenster.de>
2020-09-17 16:24:13 +0200

    Use babel coverage because v8 reports false covera

--- jest.config.js
+++ jest.config.js
@@ -24,12 +24,13 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+   coveragePathIgnorePatterns: [
+     "/node_modules/",
+     "/tests/"
+   ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
