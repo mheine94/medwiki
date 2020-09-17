4ddd98306b5413e8164d5c2cddc97bf4b4c68cb6
Maurice <maurice.heine@uni-muenster.de>
2020-09-17 14:53:04 +0200

    util tests

--- tests/util.test.js
+++ tests/util.test.js
@@ -1,10 +1,23 @@
 const {html, expectedDict} = require('./util-test-data')
 const cheerio = require('cheerio')
-const {createTableDict} = require('../src/util')
+const fetch = require('node-fetch')
+jest.mock('node-fetch')
+const {getHtml , getErrorResponse, createTableDict} = require('../src/util')
 describe('Utility function tests',()=>{
     test('Tabledict parses table as exprected', ()=>{
         let $ = cheerio.load(html)
         let dict = createTableDict($,'.infobox, wikitable')
         expect(JSON.stringify([... dict])).toBe(expectedDict)
+    })
+    test('getErrorResponse error object as expected',()=>{
+        let r = getErrorResponse("message", "query")
+        expect(r.error).toEqual("message")
+        expect(r.query).toEqual("query")
+    })
+    test('getHtml', async ()=>{
+        fetch.mockImplementation((url)=>Promise.resolve({text:jest.fn(()=>Promise.resolve(url))}))
+        let html = await getHtml("url")
+        expect(html).toEqual("url")
+        expect(fetch).toHaveBeenCalledTimes(1)
     })
 })
